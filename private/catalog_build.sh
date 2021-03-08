8547739d31208e65a24bf165d4e347bc9d5cf9f1
Gyeongyeol Choi <gyeongyeol_choi@tmax.co.kr>
2021-03-08 18:12:07 +0900

    Initial Commit

---
+++ private/catalog_build.sh
@@ -0,0 +1,6 @@
+opm alpha bundle generate --directory ./akka_1.0.0 --package akka-cluster-operator --channels stable --default stable
+docker build -t {registry}/my-manifest-bundle:0.0.1 -f bundle.Dockerfile .
+docker push {registry}/my-manifest-bundle:0.0.1
+
+opm index add --bundles {registry}/my-manifest-bundle:0.0.1 --tag {regsitry}/my-index:1.0.0 -c="docker"
+docker push {registry}/my-index:1.0.0
